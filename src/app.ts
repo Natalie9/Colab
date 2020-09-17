4493955c8acf82091befb5f4d8f6bd3d85fedad7
natalie9 <natalietolentino9@gmail.com>
2020-09-17 20:25:03 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.use('/', express.static('./../web/build/'))
+app.use('/', express.static('../../web/build/'))
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join('../../web/build/', 'index.html'))
+})
 
 export default app
