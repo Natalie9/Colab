de532ce879d6168ba96f8687a5ba61431db29412
natalie9 <natalietolentino9@gmail.com>
2020-09-18 12:27:07 -0300

    build no git??

---
+++ web/build/precache-manifest.efa31d51518c3f9481059cc2f2a26b72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f22ea8e3b8a0857c66046d0676ff26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb825e6223e5c24ebb04",
+    "url": "/static/css/main.60c4d045.chunk.css"
+  },
+  {
+    "revision": "4de009884a17b6077bc3",
+    "url": "/static/js/2.fd7d28b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fd7d28b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb825e6223e5c24ebb04",
+    "url": "/static/js/main.d8df5271.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "710c395a3a7f3a3bde20844f481ddf9e",
+    "url": "/static/media/Logo_IPElab.710c395a.png"
+  },
+  {
+    "revision": "243fcdd64a2fa20fdc8edaac62a3baeb",
+    "url": "/static/media/logo.243fcdd6.png"
+  }
+]);
