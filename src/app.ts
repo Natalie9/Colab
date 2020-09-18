2933254161324c1dd8ba2105cb75e6d69762a83b
natalie9 <natalietolentino9@gmail.com>
2020-09-18 11:19:26 -0300

    /

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,10 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 let dirname = __dirname.split('/')
-let destinho = dirname.slice(0, dirname.length-1)
+let destinho = dirname.slice(1, dirname.length-1).join('/')
 console.log({dirname})
 console.log({destinho})
-const caminho = destinho +'web/build/'
+const caminho = destinho +'/web/build/'
 console.log({caminho})
 console.log(__dirname)
 app.use('/', express.static(caminho));
