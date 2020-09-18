6c49da2e354819b6ac46130abf85ae68261925bb
natalie9 <natalietolentino9@gmail.com>
2020-09-18 12:06:14 -0300

    .

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,7 @@
 module.exports = {
    "type": process.env.DB_TYPE || "mysql",
    "host": process.env.DB_HOST || "mysql669.umbler.com",
+   "port": process.env.DB_PORT || 3606,
    "username": process.env.DB_USERNAME || "ipelab",
    "password": process.env.DB_PASSWORD || "postgres",
    "database": process.env.DB_NAME || "colab",
